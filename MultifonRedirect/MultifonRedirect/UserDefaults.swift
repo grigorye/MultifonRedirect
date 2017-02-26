f6a47ac8261d936d518bf7573c91978ed22d839e
Grigory Entin <grigory.entin@gmail.com>
2017-02-27 00:17:09 +0100

    Added lastUpdateDate.

--- MultifonRedirect/MultifonRedirect/UserDefaults.swift
+++ MultifonRedirect/MultifonRedirect/UserDefaults.swift
@@ -11,12 +11,23 @@ import Foundation
 enum DefaultsKey: String {
 	case accountNumber
 	case password
+	case lastUpdateDate
 }
 
+private let defaults = UserDefaults.standard
+
 func string(for key: DefaultsKey) -> String? {
-	return UserDefaults.standard.string(forKey: key.rawValue)
+	return defaults.string(forKey: key.rawValue)
 }
 
 func set(_ value: String?, for key: DefaultsKey) {
-	return UserDefaults.standard.set(value, forKey: key.rawValue)
+	defaults.set(value, forKey: key.rawValue)
+}
+
+func date(for key: DefaultsKey) -> Date? {
+	return defaults.object(forKey: key.rawValue) as! Date?
+}
+
+func set(_ date: Date?, for key: DefaultsKey) {
+	defaults.set(date, forKey: key.rawValue)
 }
