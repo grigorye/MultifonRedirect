ec375ab5b0900c49ad94601af3ffc13ecb81cf03
Grigory Entin <grigory.entin@gmail.com>
2017-02-26 23:14:01 +0100

    Switched to PhoneNumberKit.

--- MultifonRedirect/MultifonRedirect/PhoneNumbers.swift
+++ MultifonRedirect/MultifonRedirect/PhoneNumbers.swift
@@ -6,18 +6,32 @@
 //  Copyright © 2017 Grigory Entin. All rights reserved.
 //
 
+import PhoneNumberKit
 import Foundation
+
+let defaultRegion = "RU"
+let partialFormatter = PartialFormatter(defaultRegion: defaultRegion)
+let phoneNumberKit = PhoneNumberKit()
+
+func configurePhoneNumberTextField(_ textField: UITextField) {
+	let phoneNumberTextField = textField as! PhoneNumberTextField
+	phoneNumberTextField.defaultRegion = defaultRegion
+}
+
 
 func phoneNumberFromAccountNumber(_ accountNumber: String?) -> String? {
 	guard let accountNumber = accountNumber else {
 		return nil
 	}
-	return "+\(accountNumber)"
+	return partialFormatter.formatPartial("+\(accountNumber)")
 }
 
 func accountNumberFromPhoneNumber(_ phoneNumber: String?) -> String? {
 	guard let phoneNumber = phoneNumber else {
 		return nil
 	}
-	return phoneNumber.substring(from: phoneNumber.index(after: phoneNumber.startIndex))
+	guard let nationalNumber = try? phoneNumberKit.parse(phoneNumber).nationalNumber else {
+		return nil
+	}
+	return "7\(nationalNumber)"
 }
