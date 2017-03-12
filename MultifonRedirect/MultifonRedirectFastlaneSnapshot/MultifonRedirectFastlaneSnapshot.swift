7d2959c90cdb41367a3c90d3b4b92d3abb7ea1b0
Grigory Entin <grigory.entin@gmail.com>
2017-03-12 20:45:02 +0100

    Enabled fastlane snapshot.

---
+++ MultifonRedirect/MultifonRedirectFastlaneSnapshot/MultifonRedirectFastlaneSnapshot.swift
@@ -0,0 +1,42 @@
+//
+//  MultifonRedirectFastlaneSnapshot.swift
+//  MultifonRedirectFastlaneSnapshot
+//
+//  Created by Grigory Entin on 12.03.17.
+//  Copyright © 2017 Grigory Entin. All rights reserved.
+//
+
+import XCTest
+
+class MultifonRedirectFastlaneSnapshot: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+		let app = XCUIApplication()
+		setupSnapshot(app)
+		app.launchArguments += [
+			"-accountNumber", "79876543210",
+			"-password", "123456",
+			"-lastRouting", "2"
+		]
+		app.launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testLaunch() {
+		snapshot("0Launch")
+	}
+    
+}
