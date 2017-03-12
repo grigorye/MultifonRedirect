4e9ae17549b548cad215a952e1910cdf00dfeaf8
Grigory Entin <grigory.entin@gmail.com>
2017-03-12 11:19:06 +0100

    Segregated UIKitExtensions.swift.

---
+++ MultifonRedirect/MultifonRedirect/UIKitExtensions.swift
@@ -0,0 +1,20 @@
+//
+//  UIKitExtensions.swift
+//  MultifonRedirect
+//
+//  Created by Grigory Entin on 11.03.17.
+//  Copyright © 2017 Grigory Entin. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+
+	func present(_ error: Error, forFailureDescription failureDescription: String) {
+		let alert = UIAlertController(title: failureDescription, message: ($(error) as NSError).localizedDescription, preferredStyle: .alert)
+		typealias L = RoutingViewErrorAlertLocalized
+		alert.addAction(UIAlertAction(title: L.okButtonTitle, style: .cancel))
+		present(alert, animated: true)
+	}
+	
+}
