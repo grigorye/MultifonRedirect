92ce953f202e8a43ae17bc86c37ae4b5bdaf362b
Grigory Entin <grigory.entin@gmail.com>
2017-03-12 21:04:43 +0100

    Removed launch arguments configuration in favour of Snapfile.

--- MultifonRedirect/MultifonRedirectFastlaneSnapshot/MultifonRedirectFastlaneSnapshot.swift
+++ MultifonRedirect/MultifonRedirectFastlaneSnapshot/MultifonRedirectFastlaneSnapshot.swift
@@ -20,11 +20,6 @@ class MultifonRedirectFastlaneSnapshot: XCTestCase {
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
 		let app = XCUIApplication()
 		setupSnapshot(app)
-		app.launchArguments += [
-			"-accountNumber", "79876543210",
-			"-password", "123456",
-			"-lastRouting", "2"
-		]
 		app.launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
