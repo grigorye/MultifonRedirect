27d0b815fc579cbee71fd7142b1527fc163849ad
Grigory Entin <grigory.entin@gmail.com>
2017-03-12 11:11:18 +0100

    Added ….

--- MultifonRedirect/MultifonRedirect/ExternalDependencies.swift
+++ MultifonRedirect/MultifonRedirect/ExternalDependencies.swift
@@ -17,3 +17,36 @@ func $<T>(_ value: T, line: Int = #line, function: String = #function, column: I
 	print("• \(function).\(line).\(column):\n\(description)", terminator: "")
 	return value
 }
+
+
+//
+// The idea is borrowed from https://github.com/devxoul/Then
+//
+
+infix operator …
+
+//
+
+@discardableResult
+public func with<T: AnyObject>(_ obj: T, _ initialize: (T) throws -> Void) rethrows -> T {
+	try initialize(obj)
+	return obj
+}
+
+@discardableResult
+public func …<T: AnyObject>(obj: T, initialize: (T) throws -> Void) rethrows -> T {
+	return try with(obj, initialize)
+}
+
+//
+
+public func with<T: Any>(_ value: T, _ initialize: (inout T) throws -> Void) rethrows -> T {
+	var valueCopy = value
+	try initialize(&valueCopy)
+	return valueCopy
+}
+
+@discardableResult
+public func …<T: Any>(value: T, initialize: (inout T) throws -> Void) rethrows -> T {
+	return try with(value, initialize)
+}
