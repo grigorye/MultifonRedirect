3134b013d90f542e7004b689e5ee0955e6ca764e
Grigory Entin <eg@Mac-mini-GE.local>
2017-04-09 15:38:44 +0200

    Switched from globals to GlobalAccountHolder/AccountPossessor.

---
+++ MultifonRedirect/MultifonRedirect/AccountPossessor.swift
@@ -0,0 +1,113 @@
+//
+//  AccountPossessor.swift
+//  MultifonRedirect
+//
+//  Created by Grigory Entin on 03.04.17.
+//  Copyright © 2017 Grigory Entin. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class GlobalAccountHolder {
+
+	var accountController = GlobalAccountController() {
+		didSet {
+			accountControllerDidChange()
+			if nil != accountController {
+				accountLastRoutingDidChange()
+			}
+		}
+	}
+	
+	func login(with accountParams: AccountParams, completionHandler: @escaping (Error?) -> ()) -> CancellationToken {
+		return queryRouting(for: accountParams) { (erring) in
+			DispatchQueue.main.async {
+				guard case .some(let routing) = erring else {
+					completionHandler(erring.error!)
+					return
+				}
+				defaults.accountNumber = accountParams.accountNumber
+				defaults.password = accountParams.password
+				defaults.lastUpdateDate = Date()
+				defaults.lastRouting = routing.rawValue
+				self.accountController = GlobalAccountController()
+				completionHandler(nil)
+			}
+		}
+	}
+	
+	func logout() {
+		let action = would(.logout)
+		accountController = nil
+		defaults.accountNumber = nil
+		defaults.password = nil
+		action.succeeded()
+	}
+	
+	func accountLastRoutingDidChange() {
+		precondition(nil != accountController)
+		for i in accountPossessors {
+			i.accountLastRoutingDidChange()
+		}
+	}
+	
+	func accountNextRoutingDidChange() {
+		precondition(nil != accountController)
+		for i in accountPossessors {
+			i.accountNextRoutingDidChange()
+		}
+	}
+	
+	func accountControllerDidChange() {
+		for i in accountPossessors {
+			i.accountControllerDidChange()
+		}
+	}
+
+	var accountPossessors = [AccountPossessor]()
+	
+}
+
+
+let globalAccountHolder = GlobalAccountHolder()
+
+protocol AccountPossessor : NSObjectProtocol {
+	
+	func accountControllerDidChange()
+	func accountLastRoutingDidChange()
+	func accountNextRoutingDidChange()
+	
+}
+
+extension AccountPossessor {
+	
+	private var registeredPossessors: [AccountPossessor] {
+		get {
+			return globalAccountHolder.accountPossessors
+		}
+		set {
+			globalAccountHolder.accountPossessors = newValue
+		}
+	}
+	
+	func registerAccountPossesor() {
+		registeredPossessors.append(self)
+		accountLastRoutingDidChange()
+		accountControllerDidChange()
+	}
+	
+	func unrergisterAccountPossesor() {
+		let i = (registeredPossessors.index {$0.isEqual(self)})!
+		registeredPossessors.remove(at: i)
+	}
+	
+	var accountController: AccountController? {
+		return globalAccountHolder.accountController
+	}
+	
+	func logout() {
+		return globalAccountHolder.logout()
+	}
+	
+}
