22428c22df359671c9040773fc56acef9accfbbf
Grigory Entin <eg@Mac-mini-GE.local>
2017-04-09 23:51:19 +0200

    Added defaults.alternateAppIconsEnabled; switched to bindAccountAccessor.

--- MultifonRedirect/MultifonRedirect/AlternateAppIconController.swift
+++ MultifonRedirect/MultifonRedirect/AlternateAppIconController.swift
@@ -8,6 +8,12 @@
 
 import UIKit
 
+extension KVOCompliantUserDefaults {
+	
+	@NSManaged var alternateAppIconsEnabled: Bool
+	
+}
+
 struct AppIconNames {
 	
 	static let phoneOnly = "AppIcon-PhoneOnly"
@@ -20,8 +26,31 @@ class AlternateAppIconController : NSObject, AccountPossessor {
 	
 	let application = UIApplication.shared
 
-	func accountControllerDidChange() {
+	func clearAppIconAsUserDisabled() {
 		guard #available(iOS 10.3, *) else {
+			return
+		}
+		let action = would(.clearAppIconAsUserDisabled); let preflight: Preflight?; defer { action.preflight = preflight }
+		guard application.supportsAlternateIcons else {
+			preflight = .cancelled(due: .applicationDoesNotSupportAlternateIcons)
+			return
+		}
+		guard application.alternateIconName != nil else {
+			preflight = .cancelled(due: .applicationIconIsAlreadyPrimary)
+			return
+		}
+		preflight = nil
+		application.setAlternateIconName(nil) { error in
+			if let error = error {
+				action.failed(due: $(error))
+				return
+			}
+			action.succeeded()
+		}
+	}
+	
+	func accountControllerDidChange() {
+		guard defaults.alternateAppIconsEnabled, #available(iOS 10.3, *) else {
 			return
 		}
 		if nil == accountController {
@@ -40,7 +69,7 @@ class AlternateAppIconController : NSObject, AccountPossessor {
 	}
 	
 	func accountLastRoutingDidChange() {
-		guard #available(iOS 10.3, *) else {
+		guard defaults.alternateAppIconsEnabled, #available(iOS 10.3, *) else {
 			return
 		}
 		let lastRouting = accountController?.lastRouting
@@ -81,12 +110,35 @@ class AlternateAppIconController : NSObject, AccountPossessor {
 		scheduledForDeinit.perform()
 	}
 	
+	func invalidateApplicationIconForDefaults() {
+		guard .active == application.applicationState else {
+			let notificationCenter = NotificationCenter.default
+			var observer: NSObjectProtocol! = nil
+			observer = notificationCenter.addObserver(forName: .UIApplicationDidBecomeActive, object: nil, queue: nil) { _ in
+				self.invalidateApplicationIconForDefaults()
+				notificationCenter.removeObserver(observer)
+			}
+			return
+		}
+		if !defaults.alternateAppIconsEnabled {
+			clearAppIconAsUserDisabled()
+		}
+		else {
+			accountLastRoutingDidChange()
+		}
+	}
+	
+	private final func bindAlternateAppIconsEnabledDefault() -> Handler {
+		let binding = KVOBinding(defaults•#keyPath(KVOCompliantUserDefaults.alternateAppIconsEnabled), options: [.initial]) { (_) in
+			self.invalidateApplicationIconForDefaults()
+		}
+		return { _ = binding }
+	}
+	
 	override init() {
 		super.init()
-		self.registerAccountPossesor()
-		scheduledForDeinit.append {
-			self.unrergisterAccountPossesor()
-		}
+		scheduledForDeinit.append(bindAlternateAppIconsEnabledDefault())
+		scheduledForDeinit.append(bindAccountAccessor())
 	}
 
 }
