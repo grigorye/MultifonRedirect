ff00a29e56a27b0b7aca2549836a1021103dcd0b
Grigory Entin <eg@Mac-mini-GE.local>
2017-04-09 15:41:24 +0200

    Added icons; updated for AccountPossessor/AccountController.

--- MultifonRedirect/MultifonRedirect/ShortcutsController.swift
+++ MultifonRedirect/MultifonRedirect/ShortcutsController.swift
@@ -8,7 +8,21 @@
 
 import UIKit
 
-class ShortcutsController {
+private let selectedIcon = UIApplicationShortcutIcon(templateImageName: "Shortcut-ActiveRoute")
+private let nonselectedIcon = UIApplicationShortcutIcon(templateImageName: "Shortcut-InactiveRoute")
+
+extension Optional where Wrapped == Routing {
+	
+	func shortcutIcon(forSelected routing: Routing) -> UIApplicationShortcutIcon {
+		guard case .some(routing) = self else {
+			return nonselectedIcon
+		}
+		return selectedIcon
+	}
+	
+}
+
+class ShortcutsController : NSObject, AccountPossessor {
 	
 	typealias L = ShortcutsLocalized
 	
@@ -63,13 +77,12 @@ class ShortcutsController {
 		switch shortcut {
 		case .routing(let routing):
 			let action = would(.setRoutingFromShortcut(routing))
-			routingController.set(routing) { (error) in
+			accountController!.setRouting(routing) { (erring) in
 				DispatchQueue.main.async {
-					if let error = error {
-						action.failed(due: error)
+					guard case .some(_) = erring else {
+						action.failed(due: erring.error!)
 						return
 					}
-					updateRouting(from: routingController)
 					action.succeeded()
 				}
 			}
@@ -101,20 +114,62 @@ class ShortcutsController {
 			self.launchedShortcutItem = nil
 		}
 	}
+	
+	static let logoutIcon = UIApplicationShortcutIcon(templateImageName: "Shortcut-Logout")
+	static let loginIcon = UIApplicationShortcutIcon(templateImageName: "Shortcut-Login")
+	let logoutShortcutItem = UIMutableApplicationShortcutItem(type: Shortcut.action(.logout).type, localizedTitle: L.logoutTitle, localizedSubtitle: nil, icon: logoutIcon)
+	let loginShortcutItem = UIApplicationShortcutItem(type: Shortcut.action(.login).type, localizedTitle: L.loginTitle, localizedSubtitle: nil, icon: loginIcon)
+	
+	let phoneOnlyShortcutItem = UIMutableApplicationShortcutItem(type: Shortcut.routing(.phoneOnly).type, localizedTitle: L.phoneOnlyRoutingTitle)
+	let multifonOnlyShortcutItem = UIMutableApplicationShortcutItem(type: Shortcut.routing(.multifonOnly).type, localizedTitle: L.multifonOnlyRoutingTitle)
+	let phoneAndMultifonShortcutItem = UIMutableApplicationShortcutItem(type: Shortcut.routing(.phoneAndMultifon).type, localizedTitle: L.phoneAndMultifonRoutingTitle)
 
-	func updateShortcuts(from routingController: RoutingController?) {
+	func accountLastRoutingDidChange() {
+		let lastRouting = accountController!.lastRouting
+		phoneOnlyShortcutItem.icon = lastRouting.shortcutIcon(forSelected: .phoneOnly)
+		multifonOnlyShortcutItem.icon = lastRouting.shortcutIcon(forSelected: .multifonOnly)
+		phoneAndMultifonShortcutItem.icon = lastRouting.shortcutIcon(forSelected: .phoneAndMultifon)
+		UIApplication.shared.shortcutItems = [
+			phoneOnlyShortcutItem,
+			multifonOnlyShortcutItem,
+			phoneAndMultifonShortcutItem,
+			logoutShortcutItem
+		]
+	}
+	
+	func accountNextRoutingDidChange() {
+	}
+	
+	func accountControllerDidChange() {
 		let shortcutItems: [UIApplicationShortcutItem] = {
-			if let _ = routingController {
-				let phoneOnly = UIMutableApplicationShortcutItem(type: Shortcut.routing(.phoneOnly).type, localizedTitle: L.phoneOnlyRoutingTitle)
-				let multifonOnly = UIMutableApplicationShortcutItem(type: Shortcut.routing(.multifonOnly).type, localizedTitle: L.multifonOnlyRoutingTitle)
-				let phoneAndMultifon = UIMutableApplicationShortcutItem(type: Shortcut.routing(.phoneAndMultifon).type, localizedTitle: L.phoneAndMultifonRoutingTitle)
-				let logout = UIMutableApplicationShortcutItem(type: Shortcut.action(.logout).type, localizedTitle: L.logoutTitle)
-				return [phoneOnly, multifonOnly, phoneAndMultifon, logout]
-			} else {
-				let login = UIMutableApplicationShortcutItem(type: Shortcut.action(.login).type, localizedTitle: L.loginTitle)
-				return [login]
+			switch accountController {
+			case .some(let accountController):
+				logoutShortcutItem.localizedSubtitle = accountController.accountParams.accountNumber
+				return [
+					phoneOnlyShortcutItem,
+					multifonOnlyShortcutItem,
+					phoneAndMultifonShortcutItem,
+					logoutShortcutItem
+				]
+			case nil:
+				return [loginShortcutItem]
 			}
 		}()
 		UIApplication.shared.shortcutItems = shortcutItems
 	}
+	
+	var scheduledForDeinit = ScheduledHandlers()
+	
+	deinit {
+		scheduledForDeinit.perform()
+	}
+	
+	override init() {
+		super.init()
+		self.registerAccountPossesor()
+		scheduledForDeinit.append {
+			self.unrergisterAccountPossesor()
+		}
+	}
+	
 }
