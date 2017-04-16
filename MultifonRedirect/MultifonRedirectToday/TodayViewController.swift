08dd33dd8c13df546e1e4fb65083ffa8af43e058
Grigory Entin <eg@Mac-mini-GE.local>
2017-04-16 05:13:25 +0200

    Added route display.

--- MultifonRedirect/MultifonRedirectToday/TodayViewController.swift
+++ MultifonRedirect/MultifonRedirectToday/TodayViewController.swift
@@ -6,22 +6,27 @@
 //  Copyright © 2017 Grigory Entin. All rights reserved.
 //
 
+import MultifonRedirectSupport
+import GEFoundation
 import UIKit
 import NotificationCenter
 
 class TodayViewController: UIViewController, NCWidgetProviding {
-        
+	
+	@IBOutlet var routingLabel: UILabel!
+	@IBOutlet var notLoggedInLabel: UILabel!
+	
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view from its nib.
+		scheduledForDeinit.append(bindAccountAccessor())
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
+	
+	func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
         // Perform any setup necessary in order to update the view.
         
         // If an error is encountered, use NCUpdateResult.Failed
@@ -30,5 +35,45 @@ class TodayViewController: UIViewController, NCWidgetProviding {
         
         completionHandler(NCUpdateResult.newData)
     }
-    
+	
+	func widgetActiveDisplayModeDidChange(_ activeDisplayMode: NCWidgetDisplayMode, withMaximumSize maxSize: CGSize) {
+		_ = $(activeDisplayMode)
+	}
+	
+	var scheduledForDeinit = ScheduledHandlers()
+	deinit {
+		scheduledForDeinit.perform()
+	}
+	
+}
+
+extension TodayViewController: AccountPossessor {
+	
+	typealias L = TodayLocalized
+	
+	func accountLastRoutingDidChange() {
+		routingLabel.text = {
+			switch accountController?.lastRouting {
+			case .phoneOnly?:
+				return L.phoneOnlyRoutingTitle
+			case .multifonOnly?:
+				return L.multifonOnlyRoutingTitle
+			case .phoneAndMultifon?:
+				return L.multifonOnlyRoutingTitle
+			case nil:
+				return L.unknownRoutingTitle
+			}
+		}()
+	}
+
+	func accountControllerDidChange() {
+		let isLoggedIn = nil != accountController
+		_ = $(isLoggedIn)
+		routingLabel.isHidden = !isLoggedIn
+		notLoggedInLabel.isHidden = isLoggedIn
+	}
+
+	func accountNextRoutingDidChange() {
+	}
+	
 }
