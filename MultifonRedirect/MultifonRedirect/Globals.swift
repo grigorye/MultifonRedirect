cef3e801f936ec05a7bdaf547c0ddda7330a8ad5
Grigory Entin <eg@Mac-mini-GE.local>
2017-04-16 05:19:08 +0200

    Added defaults.

---
+++ MultifonRedirect/MultifonRedirect/Globals.swift
@@ -0,0 +1,12 @@
+//
+//  Globals.swift
+//  MultifonRedirect
+//
+//  Created by Grigory Entin on 16.04.17.
+//  Copyright © 2017 Grigory Entin. All rights reserved.
+//
+
+import class GEFoundation.TypedUserDefaults
+import Foundation
+
+public let defaults = GEFoundation.TypedUserDefaults()!
